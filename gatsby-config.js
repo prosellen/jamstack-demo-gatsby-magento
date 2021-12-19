627094ab186362f0f3cb570dbe97d3b2c8855e2b
prosellen <prosellen@users.noreply.github.com>
2021-12-19 22:58:38 +0100

    List all products and prepare individual pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,21 +24,12 @@ module.exports = {
       },
       __key: "images",
     },
-     {
-        resolve: "gatsby-source-magento2",
-        options: {
-            graphqlEndpoint: "https://master-7rqtwti-i7oni76omjsny.eu-4.magentosite.cloud/graphql",
-            // graphqlEndpoint: "https://demoshops.splendid-internet.de/magento/demoshop-magento2-daily/graphql",
-            
-            
-            // // this is optional
-            // queries: {
-            //     // see example query in src/nodes/queries/products.js
-            //     allProductsQuery: `... custom GraphQL query for fetching all the products you need to publish on Gatsby website ...`,
-            //     // see example query in src/nodes/queries/categories.js
-            //     categoryQuery: `... custom GraphQL query for fetching all the categories & product ids ...`
-            // }
-        }
+    {
+      resolve: "gatsby-source-magento2",
+      options: {
+          graphqlEndpoint: "http://master-7rqtwti-i7oni76omjsny.eu-4.magentosite.cloud/graphql",
+          // graphqlEndpoint: "https://master-7rqtwti-i7oni76omjsny.eu-4.magentosite.cloud/graphql",            
+      }
     }
   ],
 };

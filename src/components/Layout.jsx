87f4e3ab5e1ed3c9bfcd7714403b8455448ad2c8
Rosellen, Peter <prosellen@users.noreply.github.com>
2021-12-24 11:08:03 +0100

    Adds some basic layout and styling

---
+++ src/components/Layout.jsx
@@ -0,0 +1,30 @@
+import "normalize.css";
+import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
+import Typography from "../styles/Typography";
+import Footer from "./Footer";
+import Nav from "./Nav";
+
+const SiteWrapper = styled.div`
+  max-width: 100vw;
+  min-height: 100vh;
+  padding: 1rem 3rem 1rem;
+  background-color: #fff;
+  margin: 0 auto 4rem auto;
+`;
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Typography />
+      <SiteWrapper>
+        <header>Header</header>
+        <Nav />
+        {children}
+        <Footer />
+      </SiteWrapper>
+    </>
+  );
+}

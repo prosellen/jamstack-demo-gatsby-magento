5b2931a4735f3d308baa921bad1766b2662ddb2a
Peter Rosellen <prosellen@users.noreply.github.com>
2022-01-02 22:53:32 +0100

    Show product image

--- src/templates/SingleProductPage.jsx
+++ src/templates/SingleProductPage.jsx
@@ -1,15 +1,21 @@
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
 
 const SingleProductTitle = ({ name }) => {
   return <h1>{name}</h1>;
 };
 
-const SingleProductInfoPrice = ({ stockStatus }) => {
+const SingleProductInfoPrice = ({ stockStatus, amount }) => {
   return (
-    <div>
-      <p>{stockStatus ? "IN STOCK" : "OUT OF STOCK"}</p>
-    </div>
+    <>
+      <div>
+        <p>{stockStatus ? "IN STOCK" : "OUT OF STOCK"}</p>
+      </div>
+      <div>
+        As low as {amount.value} {amount.currency}
+      </div>
+    </>
   );
 };
 
@@ -21,13 +27,23 @@ const SingleProductReviewsSummary = ({ ratingSummary }) => {
   );
 };
 
+const SingleProductImage = ({ imageData }) => {
+  return <GatsbyImage image={imageData} />;
+};
+
 const SingleProductPage = ({ data }) => {
   const product = data.magentoProduct;
   return (
     <div>
       <SingleProductTitle name={product.name} />
       <SingleProductReviewsSummary ratingSummary={product.rating_summary} />
-      <SingleProductInfoPrice stockStatus={product.stock_status} />
+      <SingleProductInfoPrice
+        stockStatus={product.stock_status}
+        amount={product.price.regularPrice.amount}
+      />
+      <SingleProductImage
+        imageData={product.image.childImageSharp.gatsbyImageData}
+      />
     </div>
   );
 };
@@ -59,8 +75,26 @@ export const query = graphql`
         position
         url
       }
+      price {
+        regularPrice {
+          amount {
+            currency
+            value
+          }
+        }
+      }
+      media_gallery {
+        label
+        image {
+          childImageSharp {
+            gatsbyImageData(width: 88, layout: CONSTRAINED, height: 100)
+          }
+        }
+      }
       image {
-        url
+        childImageSharp {
+          gatsbyImageData(width: 700, layout: CONSTRAINED, height: 700)
+        }
       }
     }
   }
